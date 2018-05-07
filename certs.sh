f603b5af9d4c3588edd4f0ef2bc219cc3ef9e79a
James <james.jeffrey@oracle.com>
2018-05-07 15:44:48 -0700

    Update only renew certs if they are already there

--- certs.sh
+++ certs.sh
@@ -1,8 +1,18 @@
 #! /bin/bash -ex
-docker run -it --rm --name certbot  \
- -v ~/certs:/etc/letsencrypt   certbot/certbot certonly \
-  --manual -d *.${DOMAIN} --agree-tos --manual-public-ip-logging-ok \
-   --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
+
+if [ -d ~/certs/live/${DOMAIN} ]; then
+  echo ${DOMAIN} RENEW
+
+  docker run -it --rm --name certbot  \
+   -v ~/certs:/etc/letsencrypt   certbot/certbot renew
+else
+  echo ${DOMAIN} is new need to get certs
+
+  docker run -it --rm --name certbot  \
+   -v ~/certs:/etc/letsencrypt   certbot/certbot certonly \
+    --manual -d *.${DOMAIN} --agree-tos --manual-public-ip-logging-ok \
+     --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
+fi
 
 cp ~/certs/live/${DOMAIN}/fullchain.pem assets/client/tls/ngrokroot.crt
 
